208524d059951361dc72a163f884ab399550f51f
Nidhi1644 <kribinfo123@gmail.com>
2018-03-13 15:26:31 +0530

    step6 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    MessagesComponent,
     
   ],
   imports: [
@@ -20,6 +23,7 @@ import { HeroService } from './hero.service';
   ],
   providers: [
   HeroService,
+  MessageService,
   ],
   bootstrap: [AppComponent]
 })
