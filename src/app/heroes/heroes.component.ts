b80e6c491c6e28e5364335e8f30989e288fd3dfe
Nidhi1644 <kribinfo123@gmail.com>
2018-03-13 14:44:34 +0530

    step 6 services half part added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -17,8 +18,14 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };*/
   
-  heroes = HEROES;
+    heroes: Hero[];
   selectedHero: Hero;
+  
+  constructor(private heroService: HeroService) { }
+  
+  getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+  }
   
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
@@ -27,6 +34,7 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  this.getHeroes();
   }
 
 }
