58f5321097728b7f868408146448abd9280f14f0
Nidhi1644 <kribinfo123@gmail.com>
2018-03-13 11:53:28 +0530

    step4 click event added in ang3

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Ang3
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.2.
@@ -25,3 +26,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+=======
+# Bracket
+Bracket files Additional
+>>>>>>> 539d56f0c77d787e25aafc1c71ce7d998eb1dce7
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,11 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mrs. Nidhi' },
-  { id: 12, name: 'Mr Rahul' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Mrs Nidhi' },
+  { id: 12, name: 'Mr. Nice' },
+  { id: 13, name: 'Narco' },
+  { id: 14, name: 'Bombasto' },
+  { id: 15, name: 'Celeritas' },
+  { id: 16, name: 'Magneta' },
+  { id: 17, name: 'RubberMan' }
 ];
