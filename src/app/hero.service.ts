3e09bc9a8bb90c7f25ba3ef3b9378e9e30a65388
Nidhi1644 <kribinfo123@gmail.com>
2018-03-15 17:42:37 +0530

    final step 8

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -34,6 +34,19 @@ getHeroes (): Observable<Hero[]> {
     );
 }
  
+ /** GET hero by id. Return `undefined` when id not found */
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url)
+      .pipe(
+        map(heroes => heroes[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} hero id=${id}`);
+        }),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
  
 /** GET hero by id. Will 404 if id not found */
 getHero(id: number): Observable<Hero> {
@@ -82,7 +95,7 @@ updateHero (hero: Hero): Observable<any> {
 deleteHero (hero: Hero | number): Observable<Hero> {
   const id = typeof hero === 'number' ? hero : hero.id;
   const url = `${this.heroesUrl}/${id}`;
-
+  
   return this.http.delete<Hero>(url, httpOptions).pipe(
     tap(_ => this.log(`deleted hero id=${id}`)),
     catchError(this.handleError<Hero>('deleteHero'))
@@ -93,11 +106,14 @@ deleteHero (hero: Hero | number): Observable<Hero> {
 searchHeroes(term: string): Observable<Hero[]> {
   if (!term.trim()) {
     // if not search term, return empty hero array.
-    return of([]);
+          return of([]);
+            
   }
-  return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
+  return this.http.get<Hero[]>(`api/heroes/?name=${term}`)
+  
+  .pipe(
     tap(_ => this.log(`found heroes matching "${term}"`)),
-    catchError(this.handleError<Hero[]>('searchHeroes', []))
+     catchError(this.handleError<Hero[]>('searchHeroes', []))
   );
 }
 
